cfa83c709643a22e5278d04bbbb8d893220cc5ec
SparkChen927 <516728164@qq.com>
2022-03-29 16:51:06 +0800

    update

--- rosdep.sh
+++ rosdep.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 sudo rm /usr/lib/python3/dist-packages/rosdep2/sources_list.py
-sudo mv ./rosdep/sources_list.py /usr/lib/python3/dist-packages/rosdep2/
+sudo cp ./rosdep/sources_list.py /usr/lib/python3/dist-packages/rosdep2/
 sudo rm /usr/lib/python3/dist-packages/rosdistro/__init__.py
-sudo mv ./rosdep/__init__.py /usr/lib/python3/dist-packages/rosdistro/
+sudo cp ./rosdep/__init__.py /usr/lib/python3/dist-packages/rosdistro/
 sudo rm /usr/lib/python3/dist-packages/rosdep2/gbpdistro_support.py
-sudo mv ./rosdep/gbpdistro_support.py /usr/lib/python3/dist-packages/rosdep2/
+sudo cp ./rosdep/gbpdistro_support.py /usr/lib/python3/dist-packages/rosdep2/
 sudo rm /usr/lib/python3/dist-packages/rosdep2/rep3.py
-sudo mv ./rosdep/rep3.py /usr/lib/python3/dist-packages/rosdep2/
+sudo cp ./rosdep/rep3.py /usr/lib/python3/dist-packages/rosdep2/
 sudo rm /usr/lib/python3/dist-packages/rosdistro/manifest_provider/github.py
-sudo mv ./rosdep/github.py /usr/lib/python3/dist-packages/rosdistro/manifest_provider/
+sudo cp ./rosdep/github.py /usr/lib/python3/dist-packages/rosdistro/manifest_provider/
 echo "finish"
